9048e78727a725879962d1a181c865c088fc38e0
apiwit.p <zenzoza007@gmail.com>
2022-04-23 00:12:48 +0700

    add Modal component

---
+++ frontend/pages/components/Modal.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function Modal({
+  setShowModal,
+  title,
+  children,
+  onConfirm,
+}: any) {
+  const handleClickConfirm = () => {
+    // setShowModal(false);
+    onConfirm();
+  };
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-1/3 my-6 mx-auto max-w-3xl">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between p-4 border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-xl font-semibold">{title}</h3>
+              <button
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={() => setShowModal(false)}
+              >
+                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/*body*/}
+            {children}
+            {/*footer*/}
+            <div className="flex items-center justify-end p-6 rounded-b">
+              <button
+                className="hover:text-red-500 background-transparent px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </button>
+              <button
+                className="bg-blue-500 text-white active:bg-blue-600 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleClickConfirm}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+}
